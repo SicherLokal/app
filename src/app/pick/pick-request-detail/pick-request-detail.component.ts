c4b9cbc539a63a5e2e96e4c0180d7d03674c30fd
Nils-Börge Margotti <nilsmargotti@gmail.com>
2020-03-21 17:46:22 +0100

    Add complete button for the picker

--- src/app/pick/pick-request-detail/pick-request-detail.component.ts
+++ src/app/pick/pick-request-detail/pick-request-detail.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject, Subject } from 'rxjs';
 import { ActivatedRoute, ParamMap } from '@angular/router';
-import { switchMap, map } from 'rxjs/operators';
+import { switchMap, map, withLatestFrom, tap } from 'rxjs/operators';
 import { PickRequestService } from '../pick-request.service';
+import { SelectionModel } from '@angular/cdk/collections';
+import { MatListOption, MatSelectionListChange } from '@angular/material/list';
+import { PickRequest } from '../pick-request';
 
 @Component({
   selector: 'app-pick-request-detail',
@@ -10,15 +13,29 @@ import { PickRequestService } from '../pick-request.service';
   styleUrls: ['./pick-request-detail.component.scss']
 })
 export class PickRequestDetailComponent {
+  public selection: Subject<MatSelectionListChange> = new Subject();
 
+  public request: Observable<PickRequest>;
   public items: Observable<Array<{ id: string, name: string, amount: number }>>;
 
+  public isCompletable: Observable<boolean>;
+
   constructor(route: ActivatedRoute, pickRequestService: PickRequestService) {
-    this.items = route.paramMap.pipe(
-      switchMap((params: ParamMap) =>
-        pickRequestService.getRequest(params.get('id'))),
+    this.request = route.paramMap.pipe(
+      switchMap((params: ParamMap) => pickRequestService.getRequest(params.get('id')))
+    );
+
+    this.items = this.request.pipe(
       map((req) =>  req.items)
     );
+
+    this.isCompletable = this.selection.pipe(
+      withLatestFrom(this.items),
+      map(([change, items]) => change.source.selectedOptions.selected.length === items.length)
+    )
   }
 
+  updateSelection(update: MatSelectionListChange) {
+    this.selection.next(update);
+  }
 }
