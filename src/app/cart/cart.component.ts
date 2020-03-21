94a60375b5f9d3406537a18067bc0d4dc1aa2e6e
Matthias Bruch . netzkern <matthias.bruch@netzkern.de>
2020-03-21 17:39:10 +0100

    Cart list + localstore persisting

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { CartService } from '../cart/cart.service';
+import { ProductService } from '../products/product.service';
+import { CartItem } from './cart-item';
+import { Product } from '../products/product';
 
 @Component({
   selector: 'app-cart',
@@ -9,12 +13,27 @@ import { Router } from '@angular/router';
 
 export class CartComponent implements OnInit {
   router: Router;
+  productService: ProductService;
+  cartService: CartService;
+  cart: CartItem[];
+  productList: Product[];
 
-  constructor(_router: Router) { 
+  constructor(
+    _router: Router, 
+    _productService: ProductService, 
+    _cartService: CartService) { 
+
     this.router = _router;
+    this.productService = _productService;
+    this.cartService = _cartService;
   }
 
   ngOnInit(): void {
+    this.cart = this.cartService.getCart();
+  }
+
+  getProduct(productId: string): Product {
+    return this.productService.getProduct(productId);
   }
 
   goToCheckout(): void {
